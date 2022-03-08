a4988a1946fe7c22c77ffd5279e79ea2ce8a5d06
Dhur Rohma <99159113+dhurrohma@users.noreply.github.com>
2022-03-08 19:07:51 +0800

    Update Project_Data_Analysis_for_Retail_Supplier_Sales_Performance_Report.sql

--- SQL Project/Project Data Analysis for Retail Supplier: Sales Performance Report/Project_Data_Analysis_for_Retail_Supplier_Sales_Performance_Report.sql
+++ SQL Project/Project Data Analysis for Retail Supplier: Sales Performance Report/Project_Data_Analysis_for_Retail_Supplier_Sales_Performance_Report.sql
@@ -1,32 +1,32 @@
 -- Overall Performance by Year
 SELECT YEAR(date) AS years, SUM(sale_dollars) AS sales, COUNT(bottles_sold) AS number_of_order 
 FROM `iowa_drink_sales`
-WHERE bottles_sold > 0
+WHERE YEAR(date) < 2022 AND bottles_sold > 0
 GROUP BY YEAR(date);
 
 -- Overall Performance by Product Sub Category
 SELECT YEAR(date) AS years, category_name, SUM(sale_dollars) AS sales
 FROM `iowa_drink_sales`
-WHERE YEAR(date) >= 2020 AND YEAR(date) <= 2022 AND bottles_sold > 0
+WHERE YEAR(date) >= 2020 AND YEAR(date) <= 2021 AND bottles_sold > 0
 GROUP BY YEAR(date), category_name
 ORDER BY YEAR(date) ASC, SUM(sale_dollars) DESC;
 
--- City Store Effectiveness and Efficiency Based on Years
-SELECT YEAR(date) AS years, city, SUM(sale_dollars) AS sales
+-- Profit Percentage by Year
+SELECT YEAR(date) AS years, SUM(bottles_sold * state_bottle_cost) AS cost, SUM(sale_dollars) AS sales, ROUND((SUM(sale_dollars) - SUM(bottles_sold * state_bottle_cost)) / SUM(bottles_sold * state_bottle_cost) * 100, 2) AS profit_percentage
 FROM `iowa_drink_sales`
 WHERE bottles_sold > 0
-GROUP BY years, city
-ORDER BY years ASC, sales DESC;
+GROUP BY years
+ORDER BY years ASC;
 
--- City Store Effectiveness and Efficiency by Category in 2021
-SELECT YEAR(date) AS years, category_name, city, SUM(sale_dollars) AS sales
+-- Profit Percentage by Category
+SELECT YEAR(date) AS years, category_name, SUM(bottles_sold * state_bottle_cost) AS cost, SUM(sale_dollars) AS sales, ROUND((SUM(sale_dollars) - SUM(bottles_sold * state_bottle_cost)) / SUM(bottles_sold * state_bottle_cost) * 100, 2) AS profit_percentage
 FROM `iowa_drink_sales`
 WHERE YEAR(date) = 2021 AND bottles_sold > 0
 GROUP BY category_name
-ORDER BY sales DESC;
+ORDER BY profit_percentage DESC;
 
 -- Stores Transactions per Year
 SELECT YEAR(date) as years, COUNT(DISTINCT store_name) AS number_of_store
 FROM `iowa_drink_sales`
-WHERE bottles_sold > 0
+WHERE YEAR(date) < 2022 AND bottles_sold > 0
 GROUP BY years;
